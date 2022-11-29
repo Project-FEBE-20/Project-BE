01edd3a3ff7295c11abb55399b4a3bf257a946a4
dianaPermata <dianapermatap2120@gmail.com>
2022-11-28 22:43:11 -0800

    modify login and register,add 2 role dokter and user

--- middlewares/user.auth.js
+++ middlewares/user.auth.js
@@ -1,31 +1,39 @@
 require('dotenv').config();
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
-const verifyToken = (req, res, next) => {
-    const token = req.header('user-token')
-    if (!token) return res.status(400).json({
-        status: res.statusCode,
-        message: "Access Denied!"
-    })
+module.exports = async (req, res, next) => {
+	try {
+		if (!req.headers.authorization)
+			return res
+				.status(403)
+				.send({ status: "fail", msg: "user tidak memiliki akses" });
 
-    try {
-        const verified = jwt.verify(token, process.env.SECRET_KEY)
-        if (!verified) {
-            res.status(401).send({
-                message: "Unauthorized!",
-            });
-        } else if (verified.user._id != req.params.id) {
-            return res.status(403).json({
-                status: res.statusCode,
-                message: "Access Denied!"
-            });
-        }
-        next()
-    } catch (error) {
-        res.status(400).json({
-            status: res.statusCode,
-            message: "Invalid Token!"
-        });
-    }
-}
-module.exports = verifyToken
+		const token =
+			req.headers.authorization.split(" ")[1] || req.headers.authorization;
+
+		if (!token)
+			return res
+				.status(403)
+				.send({ status: "fail", msg: "user tidak memiliki akses" });
+		const decoded = jwt.verify(token, process.env.SECRET_KEY);
+
+		if (!decoded)
+			return res.status(401).send({ status: "fail", msg: "token tidak valid" });
+
+		if (!decoded._id)
+			return res.status(401).send({ status: "fail", msg: "token tidak valid" });
+
+		const user = await User.findOne({ _id: decoded._id }, "-password");
+
+		if (!user)
+			return res
+				.status(404)
+				.send({ status: "fail", msg: "user tidak ditemukan" });
+
+		req.user = decoded._id;
+		next();
+	} catch (error) {
+		res.status(500).send({ status: "fail", msg: error.message });
+	}
+};
--- models/user.js
+++ models/user.js
@@ -6,6 +6,7 @@ const userSchema = new Schema({
 	email: { type: String, required: true, unique: true },
 	password: { type: String, required: true, unique: true, min:6},
 	phone: { type: String, required: true },
+	isDokter:{type: Boolean, default: false},
 });
 
 const User = mongoose.model("user", userSchema);
