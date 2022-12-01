76d4b4246d66f8993d089e498486c999d85f9712
dianaPermata <113364526+dianaPermata@users.noreply.github.com>
2022-11-30 21:03:11 -0800

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,11 +1,4 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-<<<<<<< HEAD
-const db = mongoose.connect(DB_URL)
-
-module.exports = db
-=======
-
 module.exports = mongoose.connect(process.env.DB_URL);
->>>>>>> dev
