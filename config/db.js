49447fcfdd8494b70db52056f0d9449ba13ba6f3
dianaPermata <113364526+dianaPermata@users.noreply.github.com>
2022-11-30 20:41:43 -0800

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 
-const DB_URL = "mongodb://mongo:nc1S7A5GX7LiFWNcJuva@containers-us-west-94.railway.app:7485"
-
 const db = mongoose.connect(DB_URL)
 
 module.exports = db
