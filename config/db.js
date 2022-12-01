89ad9f386790e28396f0a09e27b30610e9f14fac
Hana Fauziah <102002056+hanafau@users.noreply.github.com>
2022-12-01 10:36:49 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const DB_URL = 'mongodb://localhost:27017/tanya-dokter'
+const DB_URL = "mongodb://mongo:nc1S7A5GX7LiFWNcJuva@containers-us-west-94.railway.app:7485"
 
 const db = mongoose.connect(DB_URL)
 
